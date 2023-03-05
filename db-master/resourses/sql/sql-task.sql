14de394618e9dbe4d97b86eb3a628de402dc77ce
Alexey Paramonov <paramonov_tama@mail.ru>
2023-03-05 20:49:23 +0300

    feat: 4 task

--- db-master/resourses/sql/sql-task.sql
+++ db-master/resourses/sql/sql-task.sql
@@ -29,5 +29,19 @@ SELECT ad.aircraft_code,
 FROM seats s
          JOIN aircrafts_data ad on ad.aircraft_code = s.aircraft_code
 WHERE NOT s.fare_conditions = 'Economy'
-  AND model->>'ru' LIKE '%Аэробус A321-200'
+  AND model ->> 'ru' LIKE '%Аэробус A321-200'
 ORDER BY s.seat_no;
+
+--Вывести города в которых больше 1 аэропорта ( код аэропорта, аэропорт, город)
+
+SELECT DISTINCT ad.airport_code,
+                ad.airport_name,
+                ad.city
+FROM (SELECT city, count(*)
+      FROM airports_data
+      group by city
+      HAVING count(*) > 1)
+         AS a
+         JOIN airports_data ad ON a.city = ad.city
+ORDER BY ad.city, ad.airport_name;
+
