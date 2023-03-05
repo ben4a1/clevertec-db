aaf290215ec6b265bbd92b515321fa38db304880
Alexey Paramonov <paramonov_tama@mail.ru>
2023-03-05 19:52:23 +0300

    feat: 2 task

--- db-master/resourses/sql/sql-task.sql
+++ db-master/resourses/sql/sql-task.sql
@@ -6,9 +6,32 @@ SELECT ad.model,
        (count(fare_conditions))
 FROM seats
          JOIN aircrafts_data ad on ad.aircraft_code = seats.aircraft_code
-group by ad.model, seats.fare_conditions, seats.aircraft_code
+GROUP BY ad.model, seats.fare_conditions, seats.aircraft_code
 ORDER BY aircraft_code;
 
 --Найти 3 самых вместительных самолета (модель + кол-во мест)
 
-SELECT
+SELECT ad.model,
+       (count(fare_conditions))
+FROM seats
+         JOIN aircrafts_data ad on ad.aircraft_code = seats.aircraft_code
+GROUP BY ad.model
+ORDER BY count(fare_conditions) DESC
+LIMIT 3;
+
+-- Вывести код,модель самолета и места не эконом класса
+-- для самолета 'Аэробус A321-200' с сортировкой по местам
+
+SELECT ad.aircraft_code,
+       model -> 'ru' as model,
+       s.fare_conditions,
+       s."seat_no"
+FROM seats s
+         JOIN aircrafts_data ad on ad.aircraft_code = s.aircraft_code
+WHERE NOT s.fare_conditions = 'Economy'
+  AND model = 'Аэробус A321-200'
+ORDER BY s.seat_no;
+
+SELECT model -> 'ru' as model
+FROM aircrafts_data;
+
